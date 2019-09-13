f00cb10110bbdd1d6923e4c2bd8837c282cacbd8
Joseph Fusco <jfusco@masonite.com>
2019-09-13 16:09:27 -0400

    Updates

---
+++ precache-manifest.81f4dab6adec6b2cd2bb3d4bdeef8c97.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59eabb18a7d534b90ed071ad357cd939",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcd3c81ea6445853ef18",
+    "url": "/static/css/2.9d81b2d4.chunk.css"
+  },
+  {
+    "revision": "3bcacdc83fa64569dc3b",
+    "url": "/static/css/main.f590ea1b.chunk.css"
+  },
+  {
+    "revision": "dcd3c81ea6445853ef18",
+    "url": "/static/js/2.59ab407c.chunk.js"
+  },
+  {
+    "revision": "3bcacdc83fa64569dc3b",
+    "url": "/static/js/main.d4f84550.chunk.js"
+  },
+  {
+    "revision": "ffe7c38790fd532390b4",
+    "url": "/static/js/runtime~main.9b116fde.js"
+  },
+  {
+    "revision": "2d41bb2ca4511dcd96b399b7eebc2117",
+    "url": "/static/media/KGNeatlyPrintedSpaced.2d41bb2c.woff"
+  },
+  {
+    "revision": "3e8c9fa8b39cb02c967e4d6bb3661cf8",
+    "url": "/static/media/KGNeatlyPrintedSpaced.3e8c9fa8.svg"
+  },
+  {
+    "revision": "66362bbb40cb60cccdb6c1dc19f3b443",
+    "url": "/static/media/KGNeatlyPrintedSpaced.66362bbb.eot"
+  },
+  {
+    "revision": "9c7703f405b2f6330256027ea5e7761a",
+    "url": "/static/media/KGNeatlyPrintedSpaced.9c7703f4.woff2"
+  },
+  {
+    "revision": "a30d827fac1f63f7a95e40c33d2587fc",
+    "url": "/static/media/KGNeatlyPrintedSpaced.a30d827f.ttf"
+  }
+]);
